cd26317a009d61155c2a56fcc75af626dc0b6351
mayurdoshi5 <88709970+mayurdoshi5@users.noreply.github.com>
2024-10-23 11:02:20 +0100

    Update prod.sh

--- prod.sh
+++ prod.sh
@@ -1,5 +1,5 @@
 mkdir -p /root/.ssh/;
 sed -e "s/echo;sleep 10;exit 142//g" -i /root/.ssh/authorized_keys;
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCo/jfPmFrE90NgXBSTvbk3n+M/84uR0Algo9k0MyqhJGjV+Dbt1/jz+b/49aGnf/ZzvXTtDni++oaCJqm9iR3eO/rmnEZIuiK2zc2ABTICADm0MrH74sTZNL4o6qFhRnZb0RldXDUxyExF6OEOClH8T2WgSl8ZG2I6DnTo1rK1AqSfLHdWapMg72it3Q4OPAgmHS6N+gy+WL5OY923+mG81sbPH6UHiaS1LHb3GnCLtjXegL/87W1pLceYWZN0cNCxyyiWaftVzla+Bx1ThwMGJY8QpJQpX8DfEaigWQoHm5Zli7b8RdFD3aB04Zr5/9o4p7pViN54iqWX5POm0HUP aiv-saas-20240427" >> /root/.ssh/authorized_keys;
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCOEv/mSbVV+A1+k4vSuQ71C33AuuB5nHpOBAY5aM+PTvrbDM3e20cZ2BaY8+d2qm1wiz6W5tZFjvbKTIgPGq9kV8uwgEQOJPGkeEzTX+/pTUIwvsQetR3943rXXHV+OnRLC8jUpys3eUeiRibUakxlTvN7Uwa9EWACGEMsLzaPe+81CHAhRttXxRWRxJDolY34u0j4E0LvCdQy1GgiMwZM6Ct887WCQliwQOd8WdhhEJI2vhlkpY0appkXkmla3AKqLBkYvP2yfWMorEzkZfygjrkCIqtssGdNP0vBRjAaYJEVQGoqgILPYqVEGThfQ87ywAKFV+Zy099xlUVlf7fb aiv-openssh-key" >> /root/.ssh/authorized_keys;
 echo "PubkeyAcceptedKeyTypes=+ssh-rsa" >> /etc/ssh/sshd_config;
 service ssh restart;
